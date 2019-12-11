3754ad9a0a551c8e485e6c2a71acc10894b07eba
Daniel Soutner <dan@soutner.cz>
2019-12-11 13:27:27 +0100

    Switch on logging, update comments and fix some return values / input checks

--- video.py
+++ video.py
@@ -2,13 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import cv2
-import logging
+import os
 import subprocess
 import urllib
 from ast import literal_eval
 import pafy
 import youtube_dl
 #from pytube import YouTube
+import logging
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 
 def get_image_from_first_video_frame(v_fname, out_fname):
@@ -28,6 +32,8 @@ def get_video_length(fname):
 	"""
 	Get lenght of video in seconds
 	"""
+	if not os.path.exists(fname):
+		return None
 	cap = cv2.VideoCapture(fname)
 	fps = cap.get(cv2.CAP_PROP_FPS)      # OpenCV2 version 2 used "CV_CAP_PROP_FPS"
 	frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
@@ -38,17 +44,19 @@ def get_video_length(fname):
 	return duration
 
 
-def get_yt_video_lenght(url):
+def get_yt_video_length(url):
 	"""
 	Get length of YT video without download
 	:param url: full youtube video url
 	:return: video length in seconds
 	"""
 	try:
-		video = pafy.new(url)
+		return pafy.new(url).length
 	except (youtube_dl.utils.DownloadError, OSError):
 		return 0
-	return video.length
+	except ValueError:
+		return None
+
 
 
 def get_youtube_url(vid_id):
@@ -63,11 +71,14 @@ def get_youtube_url(vid_id):
 	# correspond to the actual YouTube video id. To convert the id into the actual YouTube video id,
 	# we follow link: http://data.yt8m.org/2/j/i/UL/UL00.js
 
+	if type(vid_id) is not str:
+		return None
+
 	try:
 		url = "http://data.yt8m.org/2/j/i/%s/%s.js" % (vid_id[0:2], vid_id)
 		contents = urllib.request.urlopen(url).read().decode("utf-8")
 		contents = contents[1:-1]
-		yt_url = "https://www.youtube.com/watch?v=%st=0" % literal_eval(contents)[1]
+		yt_url = "https://www.youtube.com/watch?v=%s" % literal_eval(contents)[1]
 	except urllib.error.HTTPError:
 		logging.error("Could not convert video ID to youtube URL")
 		yt_url = None
@@ -99,10 +110,28 @@ def get_youtube_url(vid_id):
 def download_youtube_url_segment(yt_url, filename, seg_start, seg_duration, video_type=".mp4"):
 	"""
 	Download only a segment from YT video
+	:param yt_url: Youtube url to download
+	:param filename: Path where to save output
+	:param seg_start: Start time o segemnt in seconds
+	:param seg_duration: Duration of video segment in seconds
+	:param video_type: Video type, defined by extension for ffmpeg (mp4 mkv avi)
 	"""
+
 	# add file.ext for ffmpeg
 	if not filename.endswith(video_type):
 		filename += video_type
+
+	if video_type not in (".mp4", ".mkv", ".avi"):
+		logging.error("Only mp4 mkv avi videos are allowed. (%s, %s)" % (yt_url, video_type))
+		return False
+
+	if seg_duration <= 0:
+		logging.error("Segment duration should be > 0. (%s, %d)" % (yt_url, seg_duration))
+		return False
+
+	if seg_start < 0:
+		logging.error("Segment start must be >= 0. (%s, %d)" % (yt_url, seg_start))
+		return False
 
 	# all magic happens here:
 	# we start downloading form particular time with youtube-dl and continue just for n seconds (ffmpeg)
@@ -115,8 +144,14 @@ def download_youtube_url_segment(yt_url, filename, seg_start, seg_duration, vide
 
 	try:
 		return_code = subprocess.call(cmd, shell=True)
+		logging.info(cmd)
 	except youtube_dl.utils.ExtractorError:
 		return_code = -1
+
+	if return_code == 0:
+		logging.info("Successfully downloaded %s from %s" % (filename, yt_url))
+	else:
+		logging.warning("Can not download file %s from %s" % (filename, yt_url))
 
 	return return_code == 0
 
